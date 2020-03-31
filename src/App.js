30da7984d50f7301f1685943105a49ed0074a074
ENSLERMAN <marsel.garipov.rus@gmail.com>
2020-03-31 15:32:26 +0500

    add projects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Projects from "./Components/Projects"
 import Jumbotron from "./Components/Jumbotron";
 
 function App() {
   return (
     <div>
       <Jumbotron />
+      <Projects />
     </div>
   );
 }
---
+++ src/Components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Projects.scss';
+
+export default function Projects() {
+    return (
+        <div className="main">
+            <h1>Наши проекты</h1>
+            <div className="slider">
+
+            </div>
+        </div>
+    );
+}
